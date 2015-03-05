a96762c4fe0b3931e8afe30d46b6c89fad5f1e98
acs1899 <yrn19901005@gmail.com>
2015-03-05 15:55:28 +0800

    qwe

--- test.js
+++ test.js
@@ -1,10 +1,23 @@
-function* generator() {
+/*function* generator() {
     yield 'hehe';
     yield 'xixi';
 }
 
 var a = generator();
 
+/*console.log(a.next().value);
 console.log(a.next().value);
-console.log(a.next().value);
-console.log(a.next().value);
+console.log(a.next().value);*/
+
+var P = require('promise');
+
+function p(ex){
+    return new P(function(resolve,reject){
+        if(ex){
+            resolve(ex)
+        }else{
+            reject(ex);
+        }
+    });
+}
+p(1).then(function(result){console.log(result)},function(reason){console.log(reason)});
