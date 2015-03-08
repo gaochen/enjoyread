5cb612aa65e6433f26b90ccaa1948f64141ccae5
louis <hotmailgotohell@gmail.com>
2015-03-08 10:47:48 +0800

    week interface done, to to code refactoring

--- controllers/user.js
+++ controllers/user.js
@@ -4,14 +4,55 @@
     var Promise = require('promise');
     var mysql = require(__dirname + '/../core/mysql');
     var lib = require(__dirname + '/../core/lib');
+    var User = require(__dirname + '/modal/user');
+    var Setting = require(__dirname + '/modal/setting');
 
-    module.exports.before = function(req, res, next) {
+    var proto = module.exports = {};
+    proto.post = {};
+    proto.get = {};
+
+    proto.before = function(req, res, next) {
         connect = mysql.connect(__dirname + '/../mysql.json', 'enjoyread');
         if (!req.session.uid) {
-            var message = lib.genAjaxRet(10001, lib.s('ACCESS_DENY'));
-            req.end(message);
+            //for debug
+            req.session.uid = 58;
+            //debug dong
+            //var message = lib.genAjaxRet(10001, lib.s('ACCESS_DENY'));
+            //res.end(message);
         }
         next();
     }
+
+    proto.post.changepassword = function(req, res) {
+        var oldPassword = req.body.oldpassword;
+        var newPassword = req.body.newpassword;
+        var uid = req.session.uid;
+
+        var user = new User(uid);
+        User.validUser(user.email, oldPassword).then(function() {
+            user.changePassword(newPassword).then(function() {
+                res.end(lib.genAjaxRet(0));
+            }, function() {
+                res.end(lib.genAjaxRet(20001, lib.s('CHANGE_PASSWORD_FAILED')));
+            });
+        }, function() {
+            res.end(lib.genAjaxRet(20000, lib.s('INVALID_PASSWORD')));
+        });
+    }
+
+    proto.post.savesetting = function(req, res) {
+        var setting = new Setting(req.session.uid);
+        var pushtime = req.body.pushtime || '';
+        var rss = req.body.rss || [];
+
+        setting.init().then(function(setting) {
+            setting.pushtime = pushtime;
+            setting.rss = rss;
+            setting.save();
+            res.end(lib.genAjaxRet(0));
+        }, function(err) {
+            console.log(err);
+        });
+    };
 
 }());
--- core/boot.js
+++ core/boot.js
@@ -9,16 +9,16 @@
                 return;
             }
             var controller = require(__dirname + '/../controllers/' + name);
+            var controllerRealName = name.toLowerCase().replace('.js', '');
+            if (controller['before']) {
+                app.all('/' + controllerRealName + '/*', controller['before']);
+            }
             for (var method in controller) {
-                var cpath = name.toLowerCase().replace('.js', '');
-                if (controller['before']) {
-                    app.use(controller['before']);
-                }
                 for (var action in controller[method]) {
                     if (~['after', 'before'].indexOf(action)) continue;
                     var path = '/';
                     if (name !== 'index.js') {
-                        path +='/' + cpath;
+                        path += controllerRealName + '/';
                     }
                     if (action !== 'index') {
                         path += action.toLowerCase();
@@ -28,5 +28,4 @@
             }
         });
     }
-
 }());
